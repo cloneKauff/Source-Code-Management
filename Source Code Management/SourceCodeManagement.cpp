4c34d3f4859aa433a05252686698f846f891077b
19372 <19372@DESKTOP-H4N3UA0>
2023-03-16 12:32:39 -0400

    Add project files.

---
+++ Source Code Management/SourceCodeManagement.cpp
@@ -0,0 +1,15 @@
+// Morgan Kauffer
+// CIS 1202.501
+// March 16, 2023
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello, World!" << endl << endl;
+
+	system("pause");
+	return 0;
+}
